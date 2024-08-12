8a410c9e641f2199086dc41e90e7d2ed33b615b1
osawa-koki <koki@osawa.dev>
2024-08-12 21:01:21 +0900

    天気を取得する。

--- gas.ts
+++ gas.ts
@@ -1,10 +1,49 @@
 // @ts-nocheck
 
-function insertOrUpdateTextBox() {
+async function getWeatherData({ openWeatherMapApi }) {
+  const response = UrlFetchApp.fetch(`https://api.openweathermap.org/data/2.5/forecast?q=okinawa&appid=${openWeatherMapApi}`)
+  const json = JSON.parse(response.getContentText())
+  return json
+}
+
+function weatherToEmoji({ weather }) {
+  switch (weather) {
+    case 'Clear':
+      return '🌞'
+    case 'Clouds':
+      return '🌥️'
+    case 'Rain':
+      return '🌧'
+    case 'Snow':
+      return '⛄️'
+    case 'Thunderstorm':
+      return '⚡️'
+    default:
+      return '?'
+  }
+}
+
+function getWeather({ weatherData, date }) {
+  const { list } = weatherData
+  const items = list.map((item) => {
+    const givenDate = new Date(date)
+    const _date = new Date(item.dt_txt)
+    if (givenDate.getDate() !== _date.getDate()) {
+      return null
+    }
+    return weatherToEmoji({ weather: item.weather[0].main })
+  }).filter(item => item != null)
+  if (items.length === 0) return null
+  return items
+}
+
+async function main() {
   const properties = PropertiesService.getScriptProperties()
 
   const presentationId = properties.getProperty('PRESENTATION_ID')
   const slideDateMapper = JSON.parse(properties.getProperty('SLIDE_DATE_MAPPER'))
+
+  const openWeatherMapApi = properties.getProperty('OPEN_WEATHER_MAP_API')
 
   if (presentationId == null) {
     Logger.log('Presentation ID is not set.')
@@ -12,6 +51,9 @@ function insertOrUpdateTextBox() {
   }
 
   const presentation = SlidesApp.openById(presentationId)
+
+  const weatherData = await getWeatherData({ openWeatherMapApi })
+  Logger.log('Weather data: ' + JSON.stringify(weatherData))
 
   for (const [i, { slideId, date }] of Object.entries(slideDateMapper)) {
     const objectIdKey = `OBJECT_ID_${i}`
@@ -33,8 +75,12 @@ function insertOrUpdateTextBox() {
 
     let shape = objectId ? slide.getShapes().find(shape => shape.getObjectId() === objectId) : null
 
-    const text = '🌞'
-    const {x, y, width, height} = {x: 650, y: 50, width: 50, height: 50}
+    const weather = getWeather({ weatherData, date })
+
+    const text = weather != null
+      ? `${weather.join(' ')}\n(${date})`
+      : `No data\n(${date})`
+    const { x, y, width, height } = { x: 450, y: 30, width: 250, height: 50 }
 
     if (shape != null) {
       shape.getText().setText(text)
